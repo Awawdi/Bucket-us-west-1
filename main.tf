c243d7509ec49ab9c77f4bc1fc0a5037885a7afc
Orsan Awawdi <orsan_awawdi@bmc.com>
2024-01-29 16:52:44 +0200

    .

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 
 resource "aws_instance" "example_server" {
 
-  ami           = "ami-0b0ea68c435eb488d"
+  ami           = "ami-07f493412d6213de6"
 
   instance_type = "t2.micro"
 
