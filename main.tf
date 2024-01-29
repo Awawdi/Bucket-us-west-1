0ca42acf3e48ba7b92a7d7051fa876e251d8f557
Orsan Awawdi <orsan_awawdi@bmc.com>
2024-01-29 16:50:12 +0200

    added AMI

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 
 resource "aws_instance" "example_server" {
 
-  ami           = "ami-04e914639d0cca79a"
+  ami           = "ami-0b0ea68c435eb488d"
 
   instance_type = "t2.micro"
 
